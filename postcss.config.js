0f2faa48cf1e0747aed0c21b3dc352b695cb1258
DoomyCrystal <shagix.ix@gmail.com>
2021-09-21 16:32:20 +0200

    add tailwindcss + downgrade postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,6 @@
 let environment = {
     plugins: [
+        require("tailwindcss")("./app/javascript/stylesheets/tailwind.config.js"),
         require('postcss-import'),
         require('postcss-flexbugs-fixes'),
         require('postcss-preset-env')({
