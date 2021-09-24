942239926dfda9d4b7ed3d4eee79c368caeb7410
DoomyCrystal <shagix.ix@gmail.com>
2021-09-24 13:40:02 +0200

    add download-button + don't loop eventlistener

--- app/javascript/scripts/application.js
+++ app/javascript/scripts/application.js
@@ -1,27 +1,26 @@
 import 'unpoly'
+import { saveAs } from 'file-saver'
+import * as htmlToImage from 'html-to-image';
+import { toPng, toJpeg, toBlob, toPixelData, toSvg } from 'html-to-image';
 
-up.compiler('.js-screen-upload', () => {
-  const uploads = document.querySelectorAll('.js-screen-upload')
+up.compiler('.js-screen-upload', (element, screenId) => {
+  const upload = element
 
-  uploads.forEach((upload, screenId) => {
-    upload.addEventListener('change', function(e) {
-      readURL(this, screenId)
-      console.log(this, screenId)
-    })
-    upload.addEventListener('mouseover', event => {
-      document.querySelector(`#js-screen-img-${screenId}`).classList.add('bg-sky-400')
-    })
-    upload.addEventListener('mouseout', event => {
-      document.querySelector(`#js-screen-img-${screenId}`).classList.remove('bg-sky-400')
-    })
+  upload.addEventListener('change', function(e) {
+    readURL(this, screenId)
+  })
+  upload.addEventListener('mouseover', event => {
+    document.querySelector(screenId).classList.add('bg-sky-400')
+  })
+  upload.addEventListener('mouseout', event => {
+    document.querySelector(screenId).classList.remove('bg-sky-400')
   })
 
   const readURL = (input, screenId) => {
-    console.log(input, screenId)
     if (input.files && input.files[0]) {
       let reader = new FileReader()
       reader.onload = (e) => {
-        document.querySelector(`#js-screen-img-${screenId}`).src = e.target.result
+        document.querySelector(screenId).src = e.target.result
       }
       reader.readAsDataURL(input.files[0])
     }
@@ -30,5 +29,19 @@ up.compiler('.js-screen-upload', () => {
   return () => {
     element.removeEventListener('change')
   }
+})
 
+up.compiler('#js-download', (element) => {
+  const FileSaver = require('file-saver');
+  const downloadButton = element
+  const mockup = document.querySelector('#mockup')
+
+  downloadButton.addEventListener('click', event => {
+    htmlToImage.toPng(mockup, {height: 2000}).then(function (dataUrl) {
+      FileSaver.saveAs(dataUrl)
+  })
+  .catch(function (error) {
+    console.error('oops, something went wrong!', error);
+  });
+  })
 })
